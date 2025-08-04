82c1c8a5b1192c7a4e6b306a74a4eb72db07978b
artemroy19 <art.roit19@gmail.com>
2025-08-05 01:29:00 +0300

    3.3

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => ({
+
+export default defineConfig({
   plugins: [react()],
-  base: mode === 'production' ? '/quest-website/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/hb20/',
-}));
+  
+  base: '/hb20/', 
+});
